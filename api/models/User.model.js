cfd971ec28be5d5a6b84000ab315d2c4f318ab8c
pranjalr06 <pranjalrodge@gmail.com>
2024-09-06 12:02:50 +0530

    Create User model

---
+++ api/models/User.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+//create a user schema
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        require: true,
+        unique: true,   //for unique username     
+    },
+    email: {
+        type:String,
+        require: true,
+        unique: true,   //for unique username     
+    },
+    password: {
+        type:String,
+        require: true,  
+    },
+}, { timestaps: true });     //it automatically gives you the time when user logs in
+
+const User = mongoose.model("User",userSchema);
+export default User;
