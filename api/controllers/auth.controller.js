3bb80ec7b5d7a67a65c18095a8c86deebde7be0e
pranjalr06 <pranjalrodge@gmail.com>
2024-09-08 20:06:34 +0530

    Create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 
@@ -16,3 +17,24 @@ export const signup = async (req, res, next) => {
     }
    
 };
+
+//create a signin function | authorization & authentication
+
+export const signin = async (req,res,next) => {
+    const { email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)    //it compare the password with the pwd in db
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const {password:pas, ...rest} = validUser._doc;     //for hiding pwd
+        res
+            .cookie('access_token',token, {httpOnly: true})
+            .status(200)
+            .json(rest); //saving the token data in the cookie
+    } catch (error) {
+        next(error);
+    }
+} 
