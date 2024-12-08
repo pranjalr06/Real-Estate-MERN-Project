efbc1b3e5faaae5c1a05cdc5ea83b2b27123fb88
pranjalr06 <pranjalrodge@gmail.com>
2024-12-08 15:50:40 +0530

    Create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -29,3 +29,27 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+//---------------------------------------------------------------------------------------------------------
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);  //to check if listing is present
+  if(!listing) {
+    return next(errorHandler(404,'Listing not found !!'))
+  }
+
+  if(req.param.id !== listing.userRef){
+    return next(errorHandler(401,'You can only update your listing'));
+  }
+
+  try{
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new:true}
+    );
+    res.status(200) .json(updateListing);
+  } catch(error) {
+    next(error);
+  }
+};
